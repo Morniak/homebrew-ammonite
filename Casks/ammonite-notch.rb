814ea4d33aaf74a803ba1d7a2782d8c0e4aa4182
Guillaume Clédat <guillaume.cledat@gmail.com>
2025-07-16 15:51:37 +0200

    Update hash

--- Casks/ammonite-notch.rb
+++ Casks/ammonite-notch.rb
@@ -1,6 +1,6 @@
 cask "ammonite-notch" do
   version '0.9.0'
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "b9bd3de86c33701256898caa450d0270f567880f89929256f2bf580700a2a3e0"
 
   url "https://github.com/Morniak/Ammonite/releases/download/#{version}/Ammonite.zip"
   name "Ammonite"
